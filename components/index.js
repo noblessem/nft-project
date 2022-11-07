47393fe4662d165e91e95cb7a913af6017dea0f7
noblessem <ikondor@yahoo.com>
2022-11-07 17:39:27 +0100

    Starting app

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import { CircleButton, RectButton } from "./Button";
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+
+export { HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton };
